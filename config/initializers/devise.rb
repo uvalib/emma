b7dd32692131ff0e41ee5db9d2ffbded72e2ce3d
Ray Lubinsky <rwl@virginia.edu>
2019-05-07 13:37:06 -0400

    Devise and Omniauth - Initial installation of devise and omniauth-oauth2 gems.

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -260,8 +260,10 @@ Devise.setup do |config|
   # Add a new OmniAuth provider. Check the wiki for more information on setting
   # up on your models and hooks.
   # config.omniauth :github, 'APP_ID', 'APP_SECRET', scope: 'user,public_repo'
-  config.omniauth :OAuth2, ApiService::API_KEY, '',
-                  client_options: { site: ApiService::AUTH_URL }
+  config.omniauth :OAuth2, ApiService::API_KEY, '', client_options: {
+    site:        ApiService::AUTH_URL,
+    auth_scheme: :basic_auth,
+  }
 
   # ==> Warden configuration
   # If you want to use other strategies, that are not supported by Devise, or
