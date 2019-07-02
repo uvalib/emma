676498a0119534aa3cd4dfc8e7a89665f956b717
Ray Lubinsky <rwl@virginia.edu>
2019-07-02 15:48:02 -0400

    [EMMA-17] UVA typeface - Added app/assets/javascripts/shared/fonts.js to load Typekit. - Added font-family "franklin-gothic-urw" to body. - Styling adjustments to accommodate the new font.

---
+++ app/assets/javascripts/shared/fonts.js
@@ -0,0 +1,36 @@
+// app/assets/javascripts/shared/fonts.js
+//
+//= require shared/definitions
+
+/**
+ * Load Typekit fonts, adding the script if it is not already present.
+ */
+(function() {
+    const $head  = $('head');
+    let $script  = $head.find('script[src*="/use.typekit.net/"]');
+    const adding = isMissing($script);
+    if (adding) {
+        $script =
+            $('<script>')
+                .attr('src',  '//use.typekit.net/tgy5tlj.js')
+                .attr('type', 'text/javascript');
+    }
+    $script
+        .off('readystatechange', loadFonts)
+        .on( 'readystatechange', loadFonts)
+        .off('load', loadFonts)
+        .on( 'load', loadFonts);
+    if (adding) {
+        $head.append($script);
+    }
+
+    function loadFonts() {
+        try {
+            Typekit.load();
+        }
+        catch(error) {
+            console.warn('Could not load Typekit: ' + error.message);
+        }
+    }
+
+})();
