99a67ef8e620ce88c11c4c87879146e1fb38f02b
Ray Lubinsky <rwl@virginia.edu>
2020-03-25 19:18:55 -0400

    [EMMA-07] Bookshare authentication - Remove outdated fix for Bookshare redirect problem. - Enhanced logging to better support investigation on AWS.

--- lib/_trace.rb
+++ lib/_trace.rb
@@ -149,11 +149,13 @@ def __output(*args)
   # For desktop builds, if explicitly requested, copy output to the log.
   unless (level = opt[:log]).blank? || application_deployed? || !defined?(Log)
     level = Log.log_level(level, :debug)
-    Log.add(level, lines)
+    #Log.add(level, lines)
+    Log.add(level, "XX #{lines}")
   end
 
   # Emit output.
-  $stderr.puts(lines)
+  #$stderr.puts(lines)
+  $stderr.puts("YY #{lines}")
   $stderr.flush
   nil
 end
