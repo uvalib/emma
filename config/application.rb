1bd700628620d9125ccb22fc3301e11b08763afa
Ray Lubinsky <rwl@virginia.edu>
2020-05-21 23:18:03 -0400

    [EMMA-49] File download - Added 'emma-storage-staging' as the default S3 bucket.

--- config/application.rb
+++ config/application.rb
@@ -39,6 +39,11 @@ module Emma
     # Security
     # =========================================================================
 
+    # Ensures that a master key has been made available in either
+    # ENV['RAILS_MASTER_KEY'] or in config/master.key. This key is used to
+    # decrypt credentials (and other encrypted files).
+    config.require_master_key = true
+
     config.session_store(
       :cookie_store,
       same_site: :lax,
