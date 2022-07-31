014a5b1b159b01f53eb4a6d7622eb05764b5011d
Ray Lubinsky <rwl@virginia.edu>
2022-07-31 17:41:09 -0400

    [EMMA-61] Production EMMA service - Fixed default logging level in config/application.rb. - Gem updates:   * aws-partitions   1.607.0 -> 1.613.0   * aws-sdk-kms      1.57.0  -> 1.58.0   * bootsnap         1.12.0  -> 1.13.0   * faraday          2.3.0   -> 2.4.0   * faraday-net_http 2.0.3   -> 2.1.0   * good_job         3.2.0   -> 3.3.2

--- config/application.rb
+++ config/application.rb
@@ -152,7 +152,7 @@ module Emma
 
     # Use the lowest log level to ensure availability of diagnostic information
     # when problems arise.
-    config.log_level = :debug
+    config.log_level = CONSOLE_DEBUGGING ? :debug : :info
 
     # Don't colorize AWS logs.
     config.colorize_logging = !application_deployed?
