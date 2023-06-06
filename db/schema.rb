7165e1384fabf2ef8e8b2e11c10b1a9010e8bb88
Nestor Walker <naw4t@virginia.edu>
2023-06-06 14:10:37 -0400

    update schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_03_24_143723) do
+ActiveRecord::Schema[7.0].define(version: 2023_06_05_175200) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pgcrypto"
   enable_extension "plpgsql"
@@ -325,6 +325,15 @@ ActiveRecord::Schema[7.0].define(version: 2023_03_24_143723) do
     t.string "identifier"
     t.string "repository"
     t.string "repositoryRecordId"
+  end
+
+  create_table "sessions", force: :cascade do |t|
+    t.string "session_id", null: false
+    t.text "data"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["session_id"], name: "index_sessions_on_session_id", unique: true
+    t.index ["updated_at"], name: "index_sessions_on_updated_at"
   end
 
   create_table "titles", force: :cascade do |t|
