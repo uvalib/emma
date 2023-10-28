cda20194ccf4ca103ba91d790e1519d4c274f41c
Ray Lubinsky <rwl@virginia.edu>
2023-10-28 16:05:15 -0400

    [EMMA-108] Math Detective trial - Added tests for Math Detective and bibliographic lookup Tool pages.

--- test/controllers/tool_controller_test.rb
+++ test/controllers/tool_controller_test.rb
@@ -7,13 +7,76 @@ require 'test_helper'
 
 class ToolControllerTest < ActionDispatch::IntegrationTest
 
+  CONTROLLER    = :tool
+  PARAMS        = { controller: CONTROLLER }.freeze
+  OPTIONS       = { controller: CONTROLLER, expect: :success }.freeze
+
+  TEST_USERS    = ALL_TEST_USERS
+  TEST_READERS  = TEST_USERS
+  TEST_WRITERS  = TEST_USERS
+
+  READ_FORMATS  = :html
+  WRITE_FORMATS = :html
+
+  setup do
+    @readers = find_users(*TEST_READERS)
+    @writers = find_users(*TEST_WRITERS)
+  end
+
   # ===========================================================================
   # :section: Read tests
   # ===========================================================================
 
-  test 'tool index' do
-    get tool_index_url
-    assert_response :success
+  test 'tool - index' do
+    action  = :index
+    params  = PARAMS.merge(action: action)
+    options = OPTIONS.merge(action: action, test: __method__)
+
+    @readers.each do |user|
+      u_opt = options
+
+      TEST_FORMATS.each do |fmt|
+        url = url_for(**params, format: fmt)
+        opt = u_opt.merge(format: fmt)
+        get_as(user, url, **opt, only: READ_FORMATS)
+      end
+    end
+  end
+
+  test 'tool - Math Detective' do
+    action  = :md
+    params  = PARAMS.merge(action: action)
+    options = OPTIONS.merge(action: action, test: __method__)
+
+    @readers.each do |user|
+      able  = user&.present?
+      u_opt = able ? options : options.except(:controller, :action, :expect)
+
+      TEST_FORMATS.each do |fmt|
+        url = url_for(**params, format: fmt)
+        opt = u_opt.merge(format: fmt)
+        opt[:expect] ||= (fmt == :html) ? :redirect : :unauthorized
+        get_as(user, url, **opt, only: READ_FORMATS)
+      end
+    end
+  end
+
+  test 'tool - bibliographic lookup' do
+    action  = :lookup
+    params  = PARAMS.merge(action: action)
+    options = OPTIONS.merge(action: action, test: __method__)
+
+    @readers.each do |user|
+      able  = user&.present?
+      u_opt = able ? options : options.except(:controller, :action, :expect)
+
+      TEST_FORMATS.each do |fmt|
+        url = url_for(**params, format: fmt)
+        opt = u_opt.merge(format: fmt)
+        opt[:expect] ||= (fmt == :html) ? :redirect : :unauthorized
+        get_as(user, url, **opt, only: READ_FORMATS)
+      end
+    end
   end
 
 end
