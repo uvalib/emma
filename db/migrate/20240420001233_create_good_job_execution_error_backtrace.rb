dde72aa5c2918959c1037466e98804d62c31bda8
Ray Lubinsky <rwl@virginia.edu>
2024-04-20 17:48:53 -0400

    [EMMA-61] - Added database migrations for GoodJob 3.28.0.

---
+++ db/migrate/20240420001233_create_good_job_execution_error_backtrace.rb
@@ -0,0 +1,20 @@
+# db/migrate/*_create_good_job_execution_error_backtrace.rb
+#
+# frozen_string_literal: true
+# warn_indent:           true
+
+class CreateGoodJobExecutionErrorBacktrace < ActiveRecord::Migration[7.1]
+
+  def change
+    reversible do |dir|
+      dir.up do
+        # Ensure this incremental update migration is idempotent
+        # with monolithic install migration.
+        return if connection.column_exists?(:good_job_executions, :error_backtrace)
+      end
+    end
+
+    add_column :good_job_executions, :error_backtrace, :text, array: true
+  end
+
+end
