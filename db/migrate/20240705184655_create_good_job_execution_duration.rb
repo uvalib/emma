4c3ef5c63cf5221246338f4e4ff06a6b91f94b2c
Ray Lubinsky <rwl@virginia.edu>
2024-07-22 12:11:37 -0400

    [EMMA-61] Production EMMA service - Migrations for "good_job_executions" due to good_job 3.30.0. - Add dashboard performance entry for good_job 4.0.0:   * Modified app/views/good_job/shared/_navbar.erb   * Modified config/locales/good_job.en.yml

---
+++ db/migrate/20240705184655_create_good_job_execution_duration.rb
@@ -0,0 +1,20 @@
+# db/migrate/*_create_good_job_execution_duration.rb
+#
+# frozen_string_literal: true
+# warn_indent:           true
+
+class CreateGoodJobExecutionDuration < ActiveRecord::Migration[7.1]
+
+  def change
+    reversible do |dir|
+      dir.up do
+        # Ensure this incremental update migration is idempotent
+        # with monolithic install migration.
+        return if connection.column_exists?(:good_job_executions, :duration)
+      end
+    end
+
+    add_column :good_job_executions, :duration, :interval
+  end
+
+end
