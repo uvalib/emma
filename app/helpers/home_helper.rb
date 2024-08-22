d6207c3e3f35d3aa26109dd7f44724fac8c39588
Ray Lubinsky <rwl@virginia.edu>
2024-08-22 16:17:47 -0400

    [EMMA-136] Orgs on welcome page - Added list of EMMA member organizations to the welcome page. - Created HomeHelper with #orgs_count and #orgs_list.

---
+++ app/helpers/home_helper.rb
@@ -0,0 +1,61 @@
+# app/helpers/home_helper.rb
+#
+# frozen_string_literal: true
+# warn_indent:           true
+
+__loading_begin(__FILE__)
+
+# Support methods for /home pages.
+#
+module HomeHelper
+
+  include CssHelper
+  include HtmlHelper
+
+  # ===========================================================================
+  # :section:
+  # ===========================================================================
+
+  public
+
+  # Generate a display of the number of active EMMA member organizations.
+  #
+  # @param [String] css               Characteristic CSS class/selector
+  # @param [Hash]   opt               Passed to #html_span.
+  #
+  # @return [ActiveSupport::SafeBuffer]
+  #
+  def orgs_count(css: '.orgs-count', **opt)
+    prepend_css!(opt, css)
+    html_span(**opt) do
+      Org.active.count
+    end
+  end
+
+  # Generate a display list of active EMMA member organizations.
+  #
+  # @param [String] css               Characteristic CSS class/selector
+  # @param [Hash]   opt               Passed to #html_ul.
+  #
+  # @return [ActiveSupport::SafeBuffer]
+  #
+  def orgs_list(css: '.orgs-list', **opt)
+    prepend_css!(opt, css)
+    html_ul(**opt) do
+      Org.active.pluck(:long_name).sort.map { |org| html_li(org) }
+    end
+  end
+
+  # ===========================================================================
+  # :section:
+  # ===========================================================================
+
+  private
+
+  def self.included(base)
+    __included(base, self)
+  end
+
+end
+
+__loading_end(__FILE__)
