8717115c562bfa72e8ed418db0d24ff25033196e
Ray Lubinsky <rwl@virginia.edu>
2025-05-07 15:41:26 -0400

    [EMMA-149] Blacklist IP ranges - Fixed handling of BLOCKED_RANGES in config/initializers/rack_attack.rb

--- config/initializers/rack_attack.rb
+++ config/initializers/rack_attack.rb
@@ -2,21 +2,25 @@
 
 require 'rack/attack'
 
-BLOCKED_RANGES = [
-  '15.177.0.0' # 2025-05-07 *.us-west-2.compute.amazonaws.com
-].freeze
+unless defined?(BLOCKED_RANGES)
 
-BLOCKED_RANGES.each do |range|
-  case range
-    when /\.0\.0\.0$/ then range += '/8'
-    when /\.0\.0$/    then range += '/16'
-    when /\.0$/       then range += '/24'
+  BLOCKED_RANGES = [
+    '15.177.0.0' # 2025-05-07 *.us-west-2.compute.amazonaws.com
+  ].freeze
+
+  BLOCKED_RANGES.each do |range|
+    case range
+      when /\.0\.0\.0$/ then range += '/8'
+      when /\.0\.0$/    then range += '/16'
+      when /\.0$/       then range += '/24'
+    end
+    Rack::Attack.blocklist_ip(range)
   end
-  Rack::Attack.blocklist_ip(range)
-end
 
-# Using 503 because it may make attacker think that they have successfully
-# DOSed the site. Rack::Attack returns 403 for blocklists by default.
-Rack::Attack.blocklisted_responder = lambda do |_request|
-  [ 503, {}, ['Blocked']]
+  # Using 503 because it may make attacker think that they have successfully
+  # DOSed the site. Rack::Attack returns 403 for blocklists by default.
+  Rack::Attack.blocklisted_responder = lambda do |_request|
+    [ 503, {}, ['Blocked']]
+  end
+
 end
